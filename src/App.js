5c7cf719dfc46205ca97e68fa23a34f46481a07c
matib83 <efbingenieria@gmail.com>
2022-08-08 09:44:20 -0300

    Login con metodos (no es buena practica, mejor crear componentes)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([]) 
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -57,14 +62,80 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const HandleLogin = async (event) => {
+    event.preventDefault()
+    //console.log('THIS IS SUBMIT!')
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      })
+
+      console.log(user)
+      setUser(user)
+      setUsername('')
+      setPassword('') 
+    } catch(e) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
 
+  const renderLoginForm = () => {
+    return (
+      <form onSubmit={HandleLogin}>
+        <div>
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            placeholder='Username'
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            placeholder='Password'
+            onChange={({target}) => setPassword(target.value)}
+          />
+        </div>
+        <button>
+          Login
+        </button>
+      </form>  
+    )
+  }
+
+  const renderCreateNoteForm = () => {
+    return (
+      <form onSubmit={addNote}>
+      <input
+        placeholder='Write your note content'
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form> 
+    )
+  }
+
   return (
     <div>
       <h1>Notes</h1>
-      <Notification message={errorMessage} />
+      <Notification message={errorMessage} /> 
+
+      {renderLoginForm()}
+      {renderCreateNoteForm()}
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -79,13 +150,6 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>   
     </div>
   )
 }
