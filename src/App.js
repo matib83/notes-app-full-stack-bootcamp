a6dc9cb6df051b39cb110df88d9cdbc457c996cd
matib83 <efbingenieria@gmail.com>
2022-11-15 09:50:28 -0300

    Hago el manejo del login con redireccionamiento cuando inicia sesion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import Login from './Login'
 import { NoteDetail } from './components/NoteDetail'
 import Notes from './Notes'
 import noteService from './services/notes'
@@ -14,6 +15,7 @@ const inlineStyles = {
 
 const App = () => {
   const [notes, setNotes] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -23,15 +25,31 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <header>
-        <Link to='/' style={inlineStyles}>home</Link>
-        <Link to='/notes' style={inlineStyles}>notes</Link>
-        <Link to='/users' style={inlineStyles}>users</Link>
+        <Link to='/' style={inlineStyles}>Home</Link>
+        <Link to='/notes' style={inlineStyles}>Notes</Link>
+        <Link to='/users' style={inlineStyles}>Users</Link>
+        {
+          user 
+            ? <em>Logged as {user.name}</em>
+            : <Link to='/login' style={inlineStyles}>Login</Link>
+        }
+        
       </header>
 
       <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path='/notes/:noteId' element={<NoteDetail notes={notes}/>} />
         <Route path='/notes' element={<Notes />} />
         <Route path='/users' element={<Users />} />
